16f8a8054cc8a8b1babebe492d5ef1f20772a5d5
danila.bespalov@gmail.com <danila.bespalov@gmail.com@2f83b99a-b095-b21e-82d6-166a59759a13>
2011-03-09 16:27:31 +0000

    Drafted a prototype

---
+++ trunk/input.cpp
@@ -0,0 +1,42 @@
+// This file is released under the terms of the GNU General Public License v3.  // Copyright 2011  Danila Bespalov <danila.bespalov@gmail.com>,
+//                 Gleb Devlikamov <onclip@gmail.com>
+//
+// Dealing with input devices.
+
+#include "maze.h"
+
+void TheGame::ProcessEvents()
+{
+	SDL_Event event;
+	while(SDL_PollEvent(&event))
+	{
+		switch(event.type)
+		{
+		case SDL_QUIT:
+			should_stop = true;
+			break;
+		case SDL_MOUSEMOTION:
+			yaw += event.motion.xrel;
+			pitch += event.motion.yrel;
+			break;
+		case SDL_KEYDOWN:
+			SDLMod mod = SDL_GetModState();
+			switch(event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				should_stop = true;
+				break;
+			case SDLK_F4:
+				// For unknown reasons SDL applications won't respond to
+				// pressing ALT+F4, something few people would like.
+				if(mod & KMOD_ALT) should_stop = true;
+				break;
+			case SDLK_r:
+				yaw = 0;
+				pitch = 0;
+				break;
+			}
+			break;
+		}
+	}
+}
---
+++ trunk/settings.cpp
@@ -0,0 +1,21 @@
+// This file is released under the terms of GPLv3 license.
+//
+// Implementation of the MazeSettings class, loading/storing settings in a file.
+
+#include "maze.h"
+
+MazeSettings::MazeSettings()
+{
+	wnd_width = 1024;
+	wnd_height = 768;
+}
+
+void Read()
+{
+	// not implemented yet
+}
+
+void Store()
+{
+	// not implemented yet
+}
