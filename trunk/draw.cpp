4e337d54ddeb36c889be7f41c164bfbb06938724
danila.bespalov@gmail.com <danila.bespalov@gmail.com@2f83b99a-b095-b21e-82d6-166a59759a13>
2011-03-23 05:59:01 +0000

    Synchronized

--- trunk/draw.cpp
+++ trunk/draw.cpp
@@ -7,17 +7,395 @@
 #include "maze.h"
 
 
-
 void TheGame::Draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glLoadIdentity();
 
+	glColor3f(0.7, 0.5, 0.7);
+	// This requires more detalization and I want it to be lightweight.
+	//GLfloat specular[] = { 1, 1, 1, 1 };
+	//glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+	//glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 21);
+
+	// temporary
+	glTranslatef(0, -2.5, -20);
+	glRotatef(-yaw, 0, 1, 0);
+	glTranslatef(0, 0, 5);
+	glCallList(display_lists[LIST_LEFT_BRANCH]);
+	glTranslatef(-2.5 - 0.15, 0, -5);
+	glRotatef(90, 0, 1, 0);
+	glCallList(display_lists[LIST_STRAIGHT_PASS]);
+	GLfloat light0_pos[] = { 0.5, 2.5, 3.0, 1 };
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_pos);
+
 	glFlush();
 	SDL_GL_SwapBuffers();
 
 	frames_drawn++;
+}
+
+void TheGame::CreateLists()
+{
+	// For now everything is hard-coded, model loading will come if need be.
+
+	GLuint start_index = glGenLists(6);
+	for(int i = 0; i < LIST_COUNT; i++)
+		display_lists[i] = start_index + i;
+
+	// crn_off must be a multiplie of 0.1!
+	const float crn_off = 0.3; // TODO: tweak this
+	const float trn_off = 0.15; // TODO: tweak this too
+	float d, d2; // d stands for delta
+
+	// A wall
+	glNewList(display_lists[LIST_WALL], GL_COMPILE);
+	glBegin(GL_QUADS);
+	for(float i = 0; i < 10 - 0.001; i += 0.2)
+		for(float j = 0; j < 5 - 2*crn_off - 0.001; j += 0.2)
+		{
+			glVertex3f(0, 2.5 - crn_off - j, - i);
+			glVertex3f(0, 2.5 - crn_off - j - 0.2, -i);
+			glVertex3f(0, 2.5 - crn_off - j - 0.2, -i - 0.2);
+			glVertex3f(0, 2.5 - crn_off - j, -i - 0.2);
+		}
+	glEnd();
+	glEndList();
+
+	// A corner cut
+	glNewList(display_lists[LIST_CORNER], GL_COMPILE);
+	glBegin(GL_QUADS);
+	for(float i = 0; i < 10 - 0.001; i += 0.2)
+	{
+		glVertex3f(crn_off, crn_off, -i);
+		glVertex3f(0, 0, -i);
+		glVertex3f(0, 0, -i - 0.2);
+		glVertex3f(crn_off, crn_off, -i - 0.2);
+	}
+	glEnd();
+	glEndList();
+
+	// It's a plank. Nothing more than just a plank.
+	glNewList(display_lists[LIST_BRANCH_PLANK], GL_COMPILE);
+	glPushMatrix();
+	glRotatef(-90, 1, 0, 0);
+	glNormal3f(0, 0, 1);
+	d = (5 - 2*crn_off) / 10;
+	d2 = (trn_off + crn_off) / 4;
+	glBegin(GL_QUADS);
+	for(float i = 0; i < 5 - 2*crn_off - 0.001; i += d)
+		for(float j = 0; j < trn_off + crn_off - 0.001; j += d2)
+		{
+			glVertex3f(j, i, 0);
+			glVertex3f(j + d2, i, 0);
+			glVertex3f(j + d2, i + d, 0);
+			glVertex3f(j, i + d, 0);
+		}
+	glEnd();
+	glPopMatrix();
+	glEndList();
+
+	// DUDE! The current normal is transformed by the modelview matrix
+	// at the point of specifying a vertex. No. Freakin. Earlier.
+
+	// Straight pass-thru
+	glNewList(display_lists[LIST_STRAIGHT_PASS], GL_COMPILE);
+
+	glNormal3f(1, 0, 0);
+	glPushMatrix();
+	glTranslatef(-2.5, 2.5, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glNormal3f(-1, 0, 0);
+	glTranslatef(5, 0, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glPopMatrix();
+	glPushMatrix();
+	glRotatef(90, 0, 0, 1);
+	glNormal3f(1, 0, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glTranslatef(5, 0, 0);
+	glNormal3f(-1, 0, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glPopMatrix();
+
+	glNormal3f(0.7071, -0.7071, 0);
+	glPushMatrix();
+	glTranslatef(-2.5, 5 - crn_off, 0);
+	glCallList(display_lists[LIST_CORNER]);
+	glTranslatef(5, 0, 0);
+	glScalef(-1, 1, 1);
+	glCallList(display_lists[LIST_CORNER]);
+	glPopMatrix();
+	glPushMatrix();
+	glNormal3f(-0.7071, 0.7071, 0);
+	glTranslatef(2.5 - crn_off, 0, 0);
+	glCallList(display_lists[LIST_CORNER]);
+	glTranslatef(-5 + 2*crn_off, 0, 0);
+	glScalef(-1, 1, 1);
+	glCallList(display_lists[LIST_CORNER]);
+	glPopMatrix();
+
+	glEndList();
+
+	// A dead-end
+	glNewList(display_lists[LIST_DEAD_END], GL_COMPILE);
+	glCallList(display_lists[LIST_STRAIGHT_PASS]);
+	glNormal3f(0, 0, 1);
+	glBegin(GL_QUADS);
+	// crazy hack for the lazy
+	for(float i = 0; i < 5 - 0.001; i += 0.2)
+		for(float j = 0; j < 5 - 0.001; j += 0.2)
+		{
+			glVertex3f(-2.5 + j, 5 - i, -10);
+			glVertex3f(-2.5 + j, 5 - i - 0.2, -10);
+			glVertex3f(-2.5 + j + 0.2, 5 - i - 0.2, -10);
+			glVertex3f(-2.5 + j + 0.2, 5 - i, -10);
+		}
+	/*glVertex3f(-2.5 + crn_off, 5, -10);
+	glVertex3f(-2.5, 5 - crn_off, -10);
+	glVertex3f(-2.5, crn_off, -10);
+	glVertex3f(-2.5 + crn_off, 0, -10);
+	glVertex3f(2.5 - crn_off, 0, -10);
+	glVertex3f(2.5, crn_off, -10);
+	glVertex3f(2.5, 5 - crn_off, -10);
+	glVertex3f(2.5 - crn_off, 5, -10);*/
+	glEnd();
+	glEndList();
+
+	// A block used to build two-way passages
+	glNewList(display_lists[LIST_WALL_BRANCH], GL_COMPILE);
+	glNormal3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	for(float i = 0; i < 2.5 - trn_off; i += (2.5 - trn_off) / 14)
+		for(float j = 0; j < 5 - 2*crn_off; j += 0.2)
+		{
+			glVertex3f(0, 2.5 -crn_off - j, -i);
+			glVertex3f(0, 2.5 - crn_off - j - 0.2, -i);
+			glVertex3f(0, 2.5 - crn_off - j - 0.2, -i - (2.5 - trn_off) / 14);
+			glVertex3f(0, 2.5 - crn_off - j, -i - (2.5 - trn_off) / 14);
+		}
+	glEnd();
+	glPushMatrix();
+	glTranslatef(0, 0, -2.5 + trn_off);
+	glBegin(GL_QUADS);
+	glNormal3f(0.7071, 0, -0.7071);
+	d = trn_off / 5;
+	for(float i = 0; i < trn_off; i += d)
+		for(float j = 0; j < 5 - 2*crn_off; j += 0.2)
+		{
+			glVertex3f(-i, 2.5 - crn_off - j, -i);
+			glVertex3f(-i, 2.5 - crn_off - j - 0.2, -i);
+			glVertex3f(-i - d, 2.5 - crn_off - j - 0.2, -i - d);
+			glVertex3f(-i - d, 2.5 - crn_off - j, -i - d);
+		}
+	d = (crn_off + trn_off) / 5;
+	for(float i = 0; i < crn_off + trn_off; i += d)
+	{
+		glNormal3f(0, 1, 0);
+		glVertex3f(crn_off - i, -2.5, -i);
+		glVertex3f(crn_off, -2.5, -i);
+		glVertex3f(crn_off, -2.5, -i - d);
+		glVertex3f(crn_off - i -d, -2.5, -i - d);
+
+		glNormal3f(0, -1, 0);
+		glVertex3f(crn_off - i, 2.5, -i);
+		glVertex3f(crn_off, 2.5, -i);
+		glVertex3f(crn_off, 2.5, -i - d);
+		glVertex3f(crn_off - i -d, 2.5, -i - d);
+	}
+	glEnd();
+
+	// Way to go, the light will be broken on those
+	glBegin(GL_QUADS);
+	glNormal3f(cos(-M_PI/4)*cos(M_PI/4), sin(-M_PI/4), -cos(-M_PI/4)*sin(M_PI/4));
+	glVertex3f(crn_off, 2.5, 0);
+	glVertex3f(0, 2.5 - crn_off, 0);
+	glVertex3f(-trn_off, 2.5 - crn_off, -trn_off);
+	glVertex3f(-trn_off, 2.5, -trn_off - crn_off);
+
+	glNormal3f(cos(M_PI/4)*cos(M_PI/4), sin(M_PI/4), -cos(M_PI/4)*sin(M_PI/4));
+	glVertex3f(crn_off, -2.5, 0);
+	glVertex3f(0, -2.5 + crn_off, 0);
+	glVertex3f(-trn_off, -2.5 + crn_off, -trn_off);
+	glVertex3f(-trn_off, -2.5, -trn_off - crn_off);
+	glEnd();
+	glPopMatrix();
+
+	glBegin(GL_QUADS);
+	for(float i = 0; i < 2.5 - trn_off; i += (2.5 - trn_off) / 14)
+	{
+		glNormal3f(0.7071, -0.7071, 0);
+		glVertex3f(crn_off, 2.5, -i);
+		glVertex3f(0, 2.5 - crn_off, -i);
+		glVertex3f(0, 2.5 - crn_off, -i - (2.5 - trn_off) / 14);
+		glVertex3f(crn_off, 2.5, -i - (2.5 - trn_off) / 14);
+
+		glNormal3f(0.7071, 0.7071, 0);
+		glVertex3f(0, -2.5 + crn_off, -i);
+		glVertex3f(crn_off, -2.5, -i);
+		glVertex3f(crn_off, -2.5, -i - (2.5 - trn_off) / 14);
+		glVertex3f(0, -2.5 + crn_off, -i - (2.5 - trn_off) / 14);
+	}
+	glEnd();
+	glEndList();
+
+	// A passage with a left turn
+	glNewList(display_lists[LIST_LEFT_BRANCH], GL_COMPILE);
+	glPushMatrix();
+	glTranslatef(-2.5, 2.5, 0);
+	glCallList(display_lists[LIST_WALL_BRANCH]);
+	glPushMatrix();
+	glTranslatef(0, 0, -10);
+	glScalef(1, 1, -1);
+	glCallList(display_lists[LIST_WALL_BRANCH]);
+	glPopMatrix();
+	glTranslatef(5, 0, 0);
+	glNormal3f(-1, 0, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glPopMatrix();
+	glPushMatrix();
+	glRotatef(90, 0, 0, 1);
+	glNormal3f(1, 0, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glTranslatef(5, 0, 0);
+	glNormal3f(-1, 0, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(2.5, 5 - crn_off, 0);
+	glScalef(-1, 1, 1);
+	glNormal3f(0.7071, -0.7071, 0);
+	glCallList(display_lists[LIST_CORNER]);
+	glTranslatef(0, -5 + 2*crn_off, 0);
+	glScalef(1, -1, 1);
+	glCallList(display_lists[LIST_CORNER]);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-2.5, 0, -2.5 + trn_off);
+	glNormal3f(0, 1, 0);
+	// TODO: you didn't create the plank for nothing!
+	// To avoid creating yet another display list
+	for(int tr = 0; tr < 2; tr++)
+	{
+	if(tr == 1) { glTranslatef(0, 5, 0); glNormal3f(0, -1, 0); }
+	glBegin(GL_QUADS);
+	for(float i = 0; i < 5; i += 0.2)
+	{
+		glVertex3f(-trn_off, 0, -crn_off - trn_off - i);
+		glVertex3f(crn_off, 0, -crn_off - trn_off - i);
+		glVertex3f(crn_off, 0, -crn_off - trn_off - i - 0.2);
+		glVertex3f(-trn_off, 0, -crn_off - trn_off - i - 0.2);
+	}
+	glEnd();
+	}
+	glPopMatrix();
+	glEndList();
+
+	glNewList(display_lists[LIST_RIGHT_BRANCH], GL_COMPILE);
+	glPushMatrix();
+	glScalef(-1, 1, 1);
+	glCallList(display_lists[LIST_LEFT_BRANCH]);
+	glPopMatrix();
+	glEndList();
+
+	// Left turn
+	glNewList(display_lists[LIST_LEFT_TURN], GL_COMPILE);
+	glPushMatrix();
+	glTranslatef(-2.5, 2.5, 0);
+	glCallList(display_lists[LIST_WALL_BRANCH]);
+	glTranslatef(5, 0, 0);
+	glNormal3f(-1, 0, 0);
+	glScalef(1, 1, 0.75);
+	glCallList(display_lists[LIST_WALL]);
+	glTranslatef(-2.5, 2.5, 0);
+	glRotatef(90, 0, 0, 1);
+	glNormal3f(-1, 0, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glTranslatef(-5, 0, 0);
+	glNormal3f(1, 0, 0);
+	glCallList(display_lists[LIST_WALL]);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(2.5, 5 - crn_off, 0);
+	glScalef(-1, 1, 0.75);
+	glNormal3f(0.7071, -0.7071, 0);
+	glCallList(display_lists[LIST_CORNER]);
+	glTranslatef(0, -5 + 2*crn_off, 0);
+	glScalef(1, -1, 1);
+	glCallList(display_lists[LIST_CORNER]);
+	glPopMatrix();
+	glPushMatrix();
+	glTranslatef(-2.5 + crn_off, 0, -7.5);
+	glNormal3f(0, 0, 1);
+	glBegin(GL_QUADS);
+	for(float i = 0; i < 5 - crn_off - 0.001; i += 0.2)
+		for(float j = 0; j < 5 - 0.001; j += 0.2)
+		{
+			glVertex3f(i, j, 0);
+			glVertex3f(i, j + 0.2, 0);
+			glVertex3f(i + 0.2, j + 0.2, 0);
+			glVertex3f(i + 0.2, j, 0);
+		}
+	glEnd();
+
+	glTranslatef(-crn_off - trn_off, crn_off, 0);
+	glPushMatrix();
+	glRotatef(90, 1, 0, 0);
+	glCallList(display_lists[LIST_BRANCH_PLANK]);
+	glPopMatrix();
+	glTranslatef(0, -crn_off, 5 - crn_off);
+	glCallList(display_lists[LIST_BRANCH_PLANK]);
+	glTranslatef(0, 5, 0);
+	glScalef(1, -1, 1);
+	glCallList(display_lists[LIST_BRANCH_PLANK]);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-2.5 + crn_off, 2.5, -7.5);
+	glBegin(GL_TRIANGLES);
+	glNormal3f(1, 0, 0);
+	glVertex3f(0, -2.5, 0);
+	glVertex3f(0, -2.5 + crn_off, 0);
+	glVertex3f(0, -2.5, crn_off);
+	glVertex3f(0, 2.5, 0);
+	glVertex3f(0, 2.5 - crn_off, 0);
+	glVertex3f(0, 2.5, crn_off);
+	glEnd();
+	glBegin(GL_QUADS);
+	d = (crn_off + trn_off) / 5;
+	for(float i = 0; i < trn_off + crn_off - 0.001; i += d)
+	{
+		glNormal3f(0, 0.7071, 0.7071);
+		glVertex3f(-i, -2.5 + crn_off, 0);
+		glVertex3f(-i, -2.5, crn_off);
+		glVertex3f(-i - d, -2.5, crn_off);
+		glVertex3f(-i - d, -2.5 + crn_off, 0);
+
+		glNormal3f(0, -0.7071, 0.7071);
+		glVertex3f(-i, 2.5 - crn_off, 0);
+		glVertex3f(-i, 2.5, crn_off);
+		glVertex3f(-i - d, 2.5, crn_off);
+		glVertex3f(-i - d, 2.5 - crn_off, 0);
+	}
+	glEnd();
+
+	glTranslatef(-crn_off - trn_off, -2.5, 2.5);
+	glRotatef(90, 0, 1, 0);
+	glScalef(1, 1, (2.5 - trn_off) / 10);
+	glCallList(display_lists[LIST_STRAIGHT_PASS]);
+
+	glPopMatrix();
+	glEndList();
+
+
+	glNewList(display_lists[LIST_RIGHT_TURN], GL_COMPILE);
+	glPushMatrix();
+	glScalef(-1, 1, 1);
+	glCallList(display_lists[LIST_LEFT_TURN]);
+	glPopMatrix();
+	glEndList();
 }
 
 void TheGame::VideoInit()
@@ -55,6 +433,15 @@ void TheGame::VideoInit()
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	GLfloat no_ambient[] = { 0.05, 0.05, 0.05, 1 }; // well, almost
+	//glLightModelfv(GL_LIGHT_MODEL_AMBIENT, no_ambient);
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, 0);
+	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.25); // 0.07
+
+	CreateLists();
 }
 
 void TheSound::SoundInit()
@@ -85,8 +472,9 @@ void TheSound::PlaySound(const char* track, int loop)
       printf ("%s\n", alGetString (error));
     }
 }
+
 void TheSound::DieSound()
 {
 	alSourceStop(source);
-	buffer = NULL;
+	buffer = 0;
 }
